00b5797b9efbd3840a6b4b943f52718cffd7ac4f
Felipe Cardoso <felipecardoso0247@gmail.com>
2026-08-12 20:41:03 -0300

    first commit

---
+++ src/test/java/com/anime/anime_api/AnimeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anime.anime_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
